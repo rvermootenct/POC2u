b3d795ee367230a7b1e8b95f2b2804af410f64ae
ryan <ryan.v@nonacreative.com>
2020-12-04 15:31:24 +0200

    simple examples and table

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,22 @@
 class WelcomeController < ApplicationController
     def index
-        puts '--------'
-        puts session[:count]
-        puts '--------'
-        @count = session[:count].to_i
+        @increment = session[:count]
+        @students = Student.all
+        @student = Student.new
+    end
+
+    def save
+        student_params = params[:student]
+        Student.create(
+            email: student_params['email'],
+            name: student_params['name'],
+            phone_number: student_params['phone_number']
+        )
+    end
+
+    private
+
+    def student_params
+        params.require(:student).permit(:name, :email, :phone_number)
     end
 end
--- app/reflexes/welcome_reflex.rb
+++ app/reflexes/welcome_reflex.rb
@@ -2,4 +2,18 @@ class WelcomeReflex < StimulusReflex::Reflex
     def greetings
         @welcome_message = 'Welcome to stimulas reflex working'
     end
+
+    def increment
+         @count = element.dataset[:count].to_i + element.dataset[:step].to_i
+    end
+
+    def say_hello
+        @value = element.attributes[:value]
+        @name = "Hello #{@value}!"
+    end
+
+    def from_stimulus(step)
+        session[:count] = session[:count].to_i + step.to_i
+        @increment = session[:count]
+    end
 end
