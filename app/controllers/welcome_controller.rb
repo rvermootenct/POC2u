178f665ee41782d16a0e839a27f77325a60536c5
ryan <ryan.v@nonacreative.com>
2020-12-15 11:20:00 +0200

    add students form back in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,23 @@
 class WelcomeController < ApplicationController
     def index
         @increment = session[:count]
+        @students = Student.all
+        @student = Student.new
+    end
+
+    def save
+        student_params = params[:student]
+        Student.create(
+            email: student_params['email'],
+            first_name: student_params['first_name'],
+            last_name: student_params['last_name'],
+            number: student_params['number']
+        )
+    end
+
+    private
+
+    def student_params
+        params.require(:student).permit(:first_name, :last_name, :email, :number)
     end
 end
---
+++ db/migrate/20201215071721_drop_students.rb
@@ -0,0 +1,5 @@
+class DropStudents < ActiveRecord::Migration[5.2]
+  def change
+    drop_table :students
+  end
+end
