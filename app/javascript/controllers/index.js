4cea21b40aca66234a0ca7c5f907a7801a60c1d8
ryan <ryan.v@nonacreative.com>
2020-12-21 14:45:42 +0200

    working reflex

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,6 +6,9 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 import StimulusReflex from 'stimulus_reflex'
 import consumer from '../channels/consumer'
 import controller from './application_controller'
+import debounced from 'debounced'
+
+debounced.initialize()
 
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
